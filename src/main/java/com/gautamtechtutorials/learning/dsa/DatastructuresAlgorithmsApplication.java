87dcd639f5276fea2afab5587ba1301d53d9e3dd
kumargautam11 <kumargautam11gk@gmail.com>
2025-07-05 15:49:10 +0530

    Initial Commit

---
+++ src/main/java/com/gautamtechtutorials/learning/dsa/DatastructuresAlgorithmsApplication.java
@@ -0,0 +1,13 @@
+package com.gautamtechtutorials.learning.dsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatastructuresAlgorithmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatastructuresAlgorithmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gautamtechtutorials/learning/dsa/DatastructuresAlgorithmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gautamtechtutorials.learning.dsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatastructuresAlgorithmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
